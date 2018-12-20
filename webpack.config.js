caec2bba27b876b04255863d60f7d4f800b35ef3
Khoa Pham <kenjifam@gmail.com>
2018-12-20 03:43:56 +0200

    initial

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: './bundle.js'
+  },
+  devtool:'source-map',
+  resolve: {
+    extensions: ['*','.ts','.tsx','.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+}

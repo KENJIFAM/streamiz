ac3eaa01d2204c001d18ba0bd3b8f2560c18b73e
Khoa Pham <kenjifam@gmail.com>
2018-12-25 19:27:19 +0200

    show streams list

--- src/components/streams/StreamList.tsx
+++ src/components/streams/StreamList.tsx
@@ -1,7 +1,43 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+import { AppState } from '../../reducers';
+import { Stream } from '../../model/Stream';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+interface PropsStreamList {
+  fetchStreams(): Promise<void>,
+  streams: Stream[]
+}
+
+class StreamList extends React.Component<PropsStreamList, {}> {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => (
+      <div className='item' key={stream.id}>
+        <i className='large middle aligned icon camera' />
+        <div className='content'>
+          {stream.title}
+          <div className='description'>{stream.description}</div>
+        </div>
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className='ui celled list'>{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState): { streams: Stream[]} => {
+  return { streams: Object.values(state.streams) };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);

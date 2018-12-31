954cd4829559a4d20f4ac0194c11ba0bc7baedb2
Khoa Pham <kenjifam@gmail.com>
2018-12-31 08:08:28 +0200

    flv player

--- src/components/streams/StreamShow.tsx
+++ src/components/streams/StreamShow.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 import { RouteComponentProps } from 'react-router';
@@ -18,8 +19,34 @@ interface PropsFromState {
 }
 
 class StreamShow extends React.Component<PropsStreamShow & PropsFromState, {}> {
+  videoRef: React.RefObject<HTMLVideoElement>;
+  player: flv.Player;
+  
+  constructor(props: PropsStreamShow & PropsFromState) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id }: MatchProps = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -31,6 +58,7 @@ class StreamShow extends React.Component<PropsStreamShow & PropsFromState, {}> {
 
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: '100%' }} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>

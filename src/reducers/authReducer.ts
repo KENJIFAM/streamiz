7425f38eee5640f4e54b8ca9c82b58765829fbcf
Khoa Pham <kenjifam@gmail.com>
2019-01-23 01:36:26 +0200

    update reducers

--- src/reducers/authReducer.ts
+++ src/reducers/authReducer.ts
@@ -1,21 +1,22 @@
 import { ActionTypes, AuthAction } from '../actions/types';
+import { User } from '../model/User';
 
 export interface AuthState {
   isSignedIn: boolean;
-  userId: string;
+  user: User;
 }
 
 const INITIAL_STATE: AuthState = {
   isSignedIn: undefined,
-  userId: undefined
+  user: undefined
 };
 
 export default (state: AuthState = INITIAL_STATE, action: AuthAction): AuthState => {
   switch (action.type) {
     case ActionTypes.SIGN_IN:
-      return {...state, isSignedIn: true, userId: action.payload};
+      return {...state, isSignedIn: true, user: action.payload};
     case ActionTypes.SIGN_OUT:
-      return {...state, isSignedIn: false, userId: undefined};
+      return {...state, isSignedIn: false, user: undefined};
     default:
       return state;
   }
